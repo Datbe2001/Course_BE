435825d789b5d4da63e02915f31a34dc8c26ecc7
Đạt Bê <90543186+Datbe2001@users.noreply.github.com>
2023-06-30 16:57:40 +0700

    feat: cru user and create access, refresh token

--- app/crud/user.py
+++ app/crud/user.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from .base import CRUDBase
 from ..model import User
 
-from app.schemas.user import UserCreate, UserUpdate
+from app.schemas.user import UserCreate, UserUpdate, UserResponse
 
 
 logger = logging.getLogger(__name__)
@@ -18,11 +18,39 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         current_email = db.query(User).filter(User.email == email).first()
         return current_email
 
+    @staticmethod
+    def get_user_by_id(db: Session, user_id: str) -> Optional[User]:
+        current_user = db.query(User).filter(User.id == user_id).first()
+        return current_user
+    
+    @staticmethod
+    def list_users(db: Session, skip: int, limit: int) -> Optional[User]:
+        total_users = db.query(User).count()
+        list_users = db.query(User).offset(skip).limit(limit).all()
+
+        result = {
+            "total_users": total_users,
+            "list_users": list_users
+        }
+        return result
+
     def create_user(self, db: Session, create_user: UserCreate):
         current_user = User(**create_user.dict())
         db.add(current_user)
         db.commit()
         db.refresh(current_user)
+        return current_user
+    
+    def update_user(self, db: Session, current_user: str, update_user: UserUpdate):
+        result = super().update(db, obj_in=update_user, db_obj=current_user)
+        return result
+    
+    def update_user_role(self, db: Session, current_user: str, user_role: str):
+        logger.info("CRUDUser: update_user_role called.")
+        current_user.system_role = user_role
+        db.commit()
+        db.refresh(current_user)
+        logger.info("CRUDUser: update_user_role called successfully.")
         return current_user
     
 
--- app/services/user.py
+++ app/services/user.py
@@ -4,26 +4,41 @@ from fastapi import HTTPException
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.constant.app_status import AppStatus
-from app.untils import hash_lib
+from app.utils import hash_lib
+from app.core.exceptions import error_exception_handler
 
 from ..model import User
-from ..schemas import UserCreate, UserCreateParams
+from ..schemas import UserCreate, UserCreateParams, UserUpdateParams, LoginUser, UserResponse
 from ..crud.user import crud_user
 
 class UserService:
     def __init__(self, db: Session):
         self.db = db
+
+    async def get_user_by_id(self, user_id: str):
+        current_user = crud_user.get_user_by_id(db=self.db,user_id=user_id)
+        return UserResponse.from_orm(current_user)
     
+    async def list_users(self, skip: int, limit: int):
+        result = crud_user.list_users(db=self.db,skip=skip, limit=limit)
+        return result
+
+
     async def create_user(self, create_user: UserCreateParams):
 
         email_lower = create_user.email.lower()
         username_lower = create_user.username.lower()
         current_email = crud_user.get_by_email(db=self.db,email=email_lower)
         if current_email:
-            raise ValueError(AppStatus.ERROR_EMAIL_ALREADY_EXIST)
+            raise error_exception_handler(error=Exception(), app_status=AppStatus.ERROR_EMAIL_ALREADY_EXIST)
+        
+        
+        if len(create_user.password) < 6:
+            raise error_exception_handler(error=Exception(), app_status=AppStatus.ERROR_INVALID_PASSWORD_LENGTH)
         
         if create_user.password != create_user.password_confirm:
-            raise ValueError(AppStatus.ERROR_REGISTER_NOT_MATCH_PASSWORD)
+            raise error_exception_handler(error=Exception(), app_status=AppStatus.ERROR_CONFIRM_PASSWORD_DOES_NOT_MATCH)
+        
         
         create_user.password = hash_lib.hash_password(create_user.password)
 
@@ -32,4 +47,33 @@ class UserService:
                                                     username=username_lower,
                                                     email=email_lower,
                                                     hashed_password=create_user.password))
-        return result
+        return UserResponse.from_orm(result)
+    
+    async def login(self, login_request: LoginUser):
+        email_lower = login_request.email.lower()
+        current_user = crud_user.get_by_email(db=self.db, email=email_lower)
+
+        if not current_user:
+            raise error_exception_handler(error=Exception(), app_status=AppStatus.ERROR_USER_NOT_FOUND)
+        
+        if not hash_lib.verify_password(login_request.password, current_user.hashed_password):
+            raise error_exception_handler(error=Exception(), app_status=AppStatus.ERROR_PASSWORD_INVALID)
+        
+        return UserResponse.from_orm(current_user)
+    
+
+    async def update_profile(self, user_id: str, update_user: UserUpdateParams):
+        current_user = crud_user.get_user_by_id(db=self.db, user_id=user_id)
+        if not current_user:
+            raise error_exception_handler(error=Exception(), app_status=AppStatus.ERROR_USER_NOT_FOUND)
+        result = crud_user.update_user(db=self.db, current_user=current_user, update_user=update_user)
+        return UserResponse.from_orm(result)
+
+
+    async def update_user_role(self, user_id: str, user_role: str):
+        current_user = crud_user.get_user_by_id(db=self.db, user_id=user_id)
+        if not current_user:
+            raise error_exception_handler(error=Exception(), app_status=AppStatus.ERROR_USER_NOT_FOUND)
+
+        result = crud_user.update_user_role(self.db, current_user=current_user, user_role=user_role)
+        return UserResponse.from_orm(result)
