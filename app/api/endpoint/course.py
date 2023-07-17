f18d4e61bc78a4aa2b665698e91f4b789ddd5587
Đạt Bê <90543186+Datbe2001@users.noreply.github.com>
2023-07-17 12:36:04 +0700

    feat: add invite and join user in course

--- app/api/endpoint/course.py
+++ app/api/endpoint/course.py
@@ -6,7 +6,7 @@ from app.api.depend import oauth2
 from app.utils.response import make_response_object
 
 from ...schemas.course import CourseCreateParams, CourseUpdate
-from ...model.base import CourseType
+from ...model.base import CourseType, CourseRole
 from ...model import User
 from ...services import CourseService
 
@@ -60,14 +60,15 @@ async def update_course(course_id: str,
     course_service = CourseService(db=db)
 
     # authorization
-    await course_service.has_course_permissions(user_id=user.id, course_id=course_id)
+    user_course = await course_service.has_course_permissions(user_id=user.id, course_id=course_id)
 
     course_response = await course_service.update_course(course_id=course_id,
-                                                         course_update=course_update)
+                                                         course_update=course_update,
+                                                         user_course=user_course.course_role)
     return make_response_object(course_response)
 
 
-@router.delete("/course/delete/{course_id}")
+@router.delete("/course/{course_id}/delete")
 async def delete_course(course_id: str,
                         user: User = Depends(oauth2.user_manager),
                         db: Session = Depends(get_db)):
@@ -75,7 +76,39 @@ async def delete_course(course_id: str,
     course_service = CourseService(db=db)
 
     # authorization
-    await course_service.has_course_permissions(user_id=user.id, course_id=course_id)
+    user_course = await course_service.has_course_permissions(user_id=user.id, course_id=course_id)
 
-    course_response = await course_service.delete_course(course_id=course_id)
+    course_response = await course_service.delete_course(course_id=course_id, user_course=user_course.course_role)
+    return make_response_object(course_response)
+
+
+@router.post("/course/{course_id}/invite")
+async def invite_participant_to_course(
+        course_id: str,
+        course_role: CourseRole,
+        user_id: str,
+        user: User = Depends(oauth2.user_manager),
+        db: Session = Depends(get_db)):
+
+    course_service = CourseService(db=db)
+
+    # authorization
+    user_course = await course_service.has_course_permissions(user_id=user.id, course_id=course_id)
+    course_response = await course_service.invite_participant_to_course(course_id=course_id,
+                                                                        course_role=course_role,
+                                                                        user_id=user_id,
+                                                                        user_course_role=user_course.course_role)
+
+    return make_response_object(course_response)
+
+
+@router.post("/course/{course_id}/join")
+async def join_to_course(
+        course_id: str,
+        user: User = Depends(oauth2.get_current_user),
+        db: Session = Depends(get_db)):
+
+    course_service = CourseService(db=db)
+    course_response = await course_service.join_to_course(user_id=user.id, course_id=course_id)
+
     return make_response_object(course_response)
--- app/crud/user_course.py
+++ app/crud/user_course.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, Optional
 from sqlalchemy.orm import Session
 from .base import CRUDBase
 from ..model import UserCourse
+from ..model.base import CourseRole
 
 from ..schemas import UserCourseCreate, UserCourseUpdate
 
@@ -13,8 +14,26 @@ logger = logging.getLogger(__name__)
 
 
 class CRUDUserCourse(CRUDBase[UserCourse, UserCourseCreate, UserCourseUpdate]):
+
+    def get_by_course_id_user_id(self, db: Session, *, user_id: str, course_id: str):
+        user_course = db.query(UserCourse).filter(UserCourse.user_id == user_id,
+                                                  UserCourse.course_id == course_id).first()
+        return user_course
+
     def create_user_course(self, db: Session, user_course_create: Dict):
         current_user_course = UserCourse(**user_course_create.dict())
+        db.add(current_user_course)
+        db.commit()
+        db.refresh(current_user_course)
+        return current_user_course
+    
+    def invite_or_join_to_user_course(self, db: Session, user_id: str, course_id: str, course_role: CourseRole):
+        current_user_course = UserCourse(
+            id = str(uuid.uuid4()),
+            user_id = user_id,
+            course_id=course_id,
+            course_role=course_role
+        )
         db.add(current_user_course)
         db.commit()
         db.refresh(current_user_course)
